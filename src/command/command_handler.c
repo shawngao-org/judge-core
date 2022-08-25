27dc543f3d3f7a1ad8c91217eb87df4a1c2b21f0
ShawnGao <network1925@outlook.com>
2022-08-26 00:32:11 +0800

    fix some code smell. Change version number.

--- src/command/command_handler.c
+++ src/command/command_handler.c
@@ -31,7 +31,7 @@ void set_config_long_value(long *key, long value) {
  * @param _config 配置信息
  * @param _args 命令参数结构体
  */
-void command_args_handler(void *arg_table, char program_name[], struct config *_config, struct command_args *_args) {
+void command_args_handler(void *arg_table, char program_name[], struct config *_config, const struct command_args *_args) {
     if (_args->help->count > 0) {
         printf("Usage: %s\n", program_name);
         arg_print_syntax(stdout, arg_table, "\n\n");
@@ -39,15 +39,15 @@ void command_args_handler(void *arg_table, char program_name[], struct config *_
         normal_exit();
     }
     if (_args->version->count > 0) {
-        printf("Judge core version: 0.1\n");
+        printf("Judge core version: 0.1.7\n");
         normal_exit();
     }
     if (_args->seccomp_rule->count <= 0 || _args->seccomp_mode->count <= 0) {
         printf("Please specify the filename or mode of the seccomp rule.\n");
         exception_exit(WRONG_CONFIG_VALUE);
     }
-    _config->seccomp_rule_name = (char *) _args->seccomp_rule->sval[0];
-    _config->seccomp_rule_mode = (char *) _args->seccomp_mode->sval[0];
+    _config->seccomp_rule_name = _args->seccomp_rule->sval[0];
+    _config->seccomp_rule_mode = _args->seccomp_mode->sval[0];
     if (_args->max_cpu_time->count > 0) {
         set_config_int_value(&_config->max_cpu_time, _args->max_cpu_time->ival[0]);
     }
@@ -69,31 +69,31 @@ void command_args_handler(void *arg_table, char program_name[], struct config *_
     if (_args->exec_path->count <= 0) {
         exception_exit(WRONG_CONFIG_VALUE);
     }
-    _config->execute_path = (char *) _args->exec_path->sval[0];
+    _config->execute_path = _args->exec_path->sval[0];
     if (_args->input_path->count > 0) {
-        _config->input_path = (char *) _args->input_path->sval[0];
+        _config->input_path = _args->input_path->sval[0];
     }
     if (_args->output_path->count > 0) {
-        _config->output_path = (char *) _args->output_path->sval[0];
+        _config->output_path = _args->output_path->sval[0];
     }
     if (_args->err_path->count > 0) {
-        _config->error_path = (char *) _args->err_path->sval[0];
+        _config->error_path = _args->err_path->sval[0];
     }
     if (_args->seccomp_rule->count > 0) {
-        _config->seccomp_rule_name = (char *) _args->seccomp_rule->sval[0];
+        _config->seccomp_rule_name = _args->seccomp_rule->sval[0];
     }
     _config->args[0] = _config->execute_path;
     int i = 1;
     if (_args->args->count > 0) {
         for (; i < _args->args->count + 1; ++i) {
-            _config->args[i] = (char *) _args->args->sval[i - 1];
+            _config->args[i] = _args->args->sval[i - 1];
         }
     }
     _config->args[i] = NULL;
     i = 0;
     if (_args->env->count > 0) {
         for (; i < _args->env->count; ++i) {
-            _config->env[i] = (char *) _args->env->sval[i];
+            _config->env[i] = _args->env->sval[i];
         }
     }
     _config->env[i] = NULL;
